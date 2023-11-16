699c22f24eb89f7243311892ab4f5b3e7e701f14
Allison <allisonabinger@gmail.com>
2023-11-16 11:28:17 -0600

    Task 0 is complete and functional, readme added

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+    Python script that gathers data from the
+    provided REST API
+"""
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+    if len(argv) < 2:
+        exit()
+    completed = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}&completed=true"
+        .format(argv[1]))
+    completed = completed.json()
+    name = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id={}"
+        .format(argv[1]))
+    name = name.json()
+    """Calling .json"""
+    name = name[0]["name"]
+    remaining = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId={}".format(argv[1]))
+    remaining = len(remaining.json())
+    completed_list = []
+    for n in completed:
+        completed_list.append("\t {}".format(n["title"]))
+    print("Employee {} is done with tasks ({}/{}):".format(
+        name, len(completed), remaining))
+    for x in completed_list:
+        print(x)
---
+++ api/README.md
@@ -0,0 +1,17 @@
+# holbertonschool-back-end
+This repository contains various projects relating to back-end programming for APIs
+
+
+General
+- What Bash scripting should not be used for
+- What is an API
+- What is a REST API
+- What are microservices
+- What is the CSV format
+- What is the JSON format
+- Pythonic Package and module name style
+- Pythonic Class name style
+- Pythonic Variable name style
+- Pythonic Function name style
+- Pythonic Constant name style
+- Significance of CapWords or CamelCase in Python
